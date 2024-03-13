9d7fba80d20064760d5df4ea45df605c5f3dc125
gateremark <mark5gatere@gmail.com>
2024-03-13 07:13:20 +0300

    refactor: update clerk auth card

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,21 +1,29 @@
 import Logo from "@/app/assets/logo";
 import { SignIn } from "@clerk/nextjs";
+// import { dark, neobrutalism } from "@clerk/themes";
+// import { useTheme } from "next-themes";
 
 type Props = {};
 
 const Signin = (props: Props) => {
+    // const { resolvedTheme } = useTheme();
     return (
-        <div className="flex max-w-7xl mx-auto flex-col items-center py-2 min-h-screen">
-            <Logo />
-            <h1 className="text-5xl font-semibold py-4">Sign In</h1>
-            <div className="flex mx-auto justify-center items-center mt-10">
+        <div className="flex max-w-7xl mx-auto flex-col items-center py-5 min-h-screen">
+            {/* <Logo /> */}
+            <h1 className="text-5xl font-semibold py-6">Sign In</h1>
+            <div className="flex mx-auto justify-center items-center">
                 <SignIn
                     appearance={{
                         elements: {
                             formButtonPrimary:
-                                "bg-black hover:bg-gray-700 transition text-sm normal-case",
+                                " hover:bg-blue-700 transition text-sm normal-case",
                         },
                     }}
+                    // appearance={
+                    //     {
+                    //         baseTheme: dark,
+                    //     }
+                    // }
                     afterSignInUrl="/getstarted"
                 />
             </div>
